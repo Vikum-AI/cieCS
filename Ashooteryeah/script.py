9421cc7ed754e132949f7acbbfb4a0190e2231c3
THEprojectMan <amantharathnayaka2005@gmail.com>
2021-12-30 20:06:58 +0530

    done so far

--- Ashooteryeah/script.py
+++ Ashooteryeah/script.py
@@ -4,14 +4,70 @@ class vehicle:
         self.car = car
         self.minivan = minivan
         self.jeep = jeep
-    def change(self, car, minivan, jeep):
-        self.car = car
-        self.minivan = minivan
-        self.jeep = jeep
-
-
+        
 v = vehicle(10000, 15000, 20000)
-print(v.car)
+
+vehi = input("Enter the type of vehicle you would like to buy: ")
+if vehi == "car":
+    print(v.car)
+elif vehi == "minivan":
+    print(v.minivan)
+elif vehi == "jeep":
+    print(v.jeep)
+else:
+    print("ERROR")
+
+opt = input("would you like to change your decision? ")
+if opt == "yes":
+    done_choosing = False
+elif opt == "no":
+    done_choosing = True
+
+while done_choosing == False:
+    vehi = input("Enter the type of vehicle you would like to buy: ")
+    if vehi == "car":
+        print(v.car)
+        opt = input("would you like to change your decision? ")
+        if opt == "yes":
+            done_choosing == False
+        elif opt == "no":
+            done_choosing == True
+    elif vehi == "minivan":
+        print(v.minivan)
+        opt = input("would you like to change your decision? ")
+        if opt == "yes":
+            done_choosing == False
+        elif opt == "no":
+            done_choosing == True
+    elif vehi == "jeep":
+        print(v.jeep)
+        opt = input("would you like to change your decision? ")
+        if opt == "yes":
+            done_choosing == False
+        elif opt == "no":
+            done_choosing == True
+    else:
+        print("ERROR")
+
+if opt == "no":
+    print("You have officially chosen your vehicle")
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+    
+
 
 
 
