2da6a31aa5275cd3d2de59f517533d202990ed50
Vikum-AI <63595074+Vikum-AI@users.noreply.github.com>
2021-12-30 12:37:09 +0530

    OOP program to create a profile and change it.

--- Destroyervik/script.py
+++ Destroyervik/script.py
@@ -1,1 +1,21 @@
-print("Hello world")
+# OOP  program that has methods; reverse an array, find an element, bubble sort.
+
+class profile:
+
+    def __init__(self, name, age, sub):
+        self.name = name
+        self.age = age
+        self.sub = sub
+
+    def change(self, name, age, sub):
+        self.name = name
+        self.age = age
+        self.sub = sub
+
+
+d = profile('Elon', 35, 'physics')
+
+print(d.name, d.age, d.sub)
+
+d.change('Mark', 37, 'Facebook')
+print(d.name, d.age, d.sub)
